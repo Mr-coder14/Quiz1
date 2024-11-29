2fa18f61db913c2caa39e89aa2b60918c6b4c809
PARMUGILAN S <135542869+Mr-coder14@users.noreply.github.com>
2024-11-29 23:21:52 +0530

    Update ResultFragment.java
    fd

--- app/src/main/java/Fragments/ResultFragment.java
+++ app/src/main/java/Fragments/ResultFragment.java
@@ -11,6 +11,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ProgressBar;
+//lavadi
 import android.widget.ScrollView;
 import android.widget.TextView;
 import android.widget.Toast;
