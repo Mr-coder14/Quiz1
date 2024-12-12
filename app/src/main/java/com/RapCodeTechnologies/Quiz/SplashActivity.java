cc1c3c4277835b613337f5331dedfb28f450735b
Shyam <shyamnagarajan325@gamil.com>
2024-12-12 13:06:35 +0530

    kg

--- app/src/main/java/com/RapCodeTechnologies/Quiz/SplashActivity.java
+++ app/src/main/java/com/RapCodeTechnologies/Quiz/SplashActivity.java
@@ -3,6 +3,8 @@ package com.RapCodeTechnologies.Quiz;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.View;
+import android.widget.ProgressBar;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.app.AppCompatDelegate;
@@ -12,27 +14,38 @@ import com.google.firebase.auth.FirebaseUser;
 
 public class SplashActivity extends AppCompatActivity {
     private FirebaseAuth auth;
+    private ProgressBar progressBar;
+    private boolean isActivityStarted = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
         AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+
+        // Initialize Firebase Auth
         auth = FirebaseAuth.getInstance();
 
+        // Initialize ProgressBar
+
+
         new Handler().postDelayed(() -> {
-            FirebaseUser userid = auth.getCurrentUser();
-            Intent intent;
-            if (userid != null) {
-                intent = new Intent(SplashActivity.this, UserMainActivity.class);
-            } else {
-                intent = new Intent(SplashActivity.this, LoginActivity.class);
+            if (!isActivityStarted) {
+                FirebaseUser userid = auth.getCurrentUser();
+                Intent intent;
+
+                if (userid != null) {
+                    intent = new Intent(SplashActivity.this, UserMainActivity.class);
+                } else {
+                    intent = new Intent(SplashActivity.this, LoginActivity.class);
+                }
+
+                // Ensure no duplicate activity launches
+                isActivityStarted = true;
+                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                startActivity(intent);
+                finish();
             }
-
-
-            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
-            startActivity(intent);
-            finish();
         }, 3000);
     }
 }
